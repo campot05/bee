155a580df4ab9da05e81c206d9ca4e7f3a7877e0
Danylo Trutniev <daniil.spectr@gmail.com>
2023-04-26 18:57:28 +0300

    add close modal

--- js/script.js
+++ js/script.js
@@ -25,3 +25,20 @@ refs.beeLink.addEventListener('click', () => {
   refs.beeLink.classList.toggle('is-open');
   refs.menuSubList.classList.toggle('show');
 });
+
+const openModal = () => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+  }
+};
+
+openModal();
